fbd5abab1d13331b457e3b0fe159c1703efe241a
Mateusz Kupczak <mateusz49@o2.pl>
2017-03-02 23:41:54 +0100

    GiM - lab. nr 1

---
+++ lab1/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    // Reference the player
+    public GameObject player;
+    // Position of our camera
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // Where we moved the camera in our scene.
+        offset = transform.position;
+	}
+	
+	// LateUpdate is called once per frame
+    // But at the end of a frame.
+	void LateUpdate () {
+        // Our initial position, plus the updated player position.
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ lab1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody playerRigidbody;
+    private int count;
+
+	// Use this for initialization
+	void Start () {
+        playerRigidbody = GetComponent<Rigidbody>();
+        count = 0;
+        RefreshCountText();
+        winText.text = string.Empty;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Record input from our player.
+        float moreHorizontal = Input.GetAxis("Horizontal");
+        float moreVertical = Input.GetAxis("Vertical");
+
+        // Use the input we get to move the rigid body.
+        Vector3 movement = new Vector3(moreHorizontal, 0.0f, moreVertical);
+        // Make a smooth movement by multiplying by Time.deltaTime.
+        // Adjust our 'movement' vector by 'speed' to make game playable.
+        playerRigidbody.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if our object collides with a pickup object.
+        if (other.gameObject.tag == "PickUp Type 1")
+        {
+            HandleObjectCollision(other.gameObject, 1);
+        }
+        else if (other.gameObject.tag == "PickUp Type 2")
+        {
+            HandleObjectCollision(other.gameObject, 4);
+        }
+    }
+
+    private void RefreshCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+    }
+
+    private void HandleObjectCollision(GameObject currentGameObject, short addToCount)
+    {
+        currentGameObject.SetActive(false);
+        count += addToCount;
+        RefreshCountText();
+        if (count >= 9)
+        {
+            winText.text = "Congratulations, you've won!";
+        }
+    }
+}
